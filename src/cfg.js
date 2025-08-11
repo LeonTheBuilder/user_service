040c97626a7534f62cf3f8cf26dbe4d0232f88d8
chence <leoooon@foxmail.com>
2025-08-11 19:28:23 +0800

    cc

--- src/cfg.js
+++ src/cfg.js
@@ -2,20 +2,11 @@ const path = require("path");
 const cfgdef = require('aframework/cfgdef');
 const userServiceSet = require('./cfgset');
 const cfg = cfgdef();
-//----------------------------------------------------------------
-//----------------------------------------------------------------
-const nodeModulesPath = path.join(__dirname, "..", 'node_modules');
-cfg.nodeModulesPath = nodeModulesPath; // ejs 页面里面使用了这个路径
-cfg.loadContextFilePath = path.join(__dirname, 'loadcontext.js');
-cfg.genFolder = path.join(__dirname, 'gen');
-cfg.typeJsFolder = __dirname;
+cfg.app.rootFolder = path.join(__dirname, '..');
 cfg.autowire.folders = [
     __dirname,
 ];
 //----------------------------------------------------------------
-cfg.web.view.apiStubFolder = require('./web/views/viewfolder');
-cfg.web.view.staticFolder = path.join(nodeModulesPath, "web_resources/src/web/public");
-console.log(cfg.web.view.staticFolder)
 cfg.mysql.database = 'user_service';
 //----------------------------------------------------------------
 userServiceSet(cfg);
--- src/loadcontext.js
+++ src/loadcontext.js
@@ -1,8 +1,11 @@
+const a = require("aframework");
 const loadContext = async function () {
     const a = require('aframework');
     const path = require("path");
     a.initEnv(path.join(__dirname, 'dev.env'));
     const cfg = require('./cfg');
+    console.log(cfg.app.rootFolder);
+
     await a.loadContext(cfg);
     return a;
 }
